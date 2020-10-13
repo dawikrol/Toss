ebddbcc7b062421ec724e21e9da04626a038ae44
dawidkrol <dawikrol@gmail.com>
2020-10-13 22:42:21 +0200

    Added try catch to MessageManager

--- Functionality/MessageManager.py
+++ Functionality/MessageManager.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 
+from GUI.InfoBoxGUI import InfoBoxGUI
 from Model.User import User
 
 
@@ -8,7 +9,6 @@ class MessageManager:
 
     def __init__(self):
         pass
-
 
     def send_list(self, title, items):
         body = f'{title}\n\n'
@@ -19,10 +19,11 @@ class MessageManager:
         msg['From'] = 'menagerlists@gmail.com'
         msg['To'] = f'{User.current_logged.email_address}'
         msg.set_content(f'{body}')
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login('menagerlists@gmail.com', 'ovoneL09')
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login('menagerlists@gmail.com', 'ovoneL09')
+                smtp.send_message(msg)
+        except Exception as e:
+            error = f"Mail could not be sent. \n Check internet connection. \n\n Error: {e}"
+            InfoBoxGUI().error_box(error)
 
-            # first is sender
-            smtp.send_message(msg)
-
-#MessageManager()
